1d859c77848c07e57aeff7e9dbfd0c9523433a73
maria <magsend@gmail.com>
2015-02-11 21:11:15 -0800

    added return statement

--- cqlengine/utils.py
+++ cqlengine/utils.py
@@ -1,5 +1,5 @@
 # adopted to python 2.6 due to lack of method "total_seconds"
 # https://docs.python.org/2/library/datetime.html#datetime.timedelta.total_seconds
 def total_seconds(value):
-     if hasattr(value, "total_seconds"): offset = value.total_seconds() 
-     else: offset = (value.microseconds+(value.seconds+value.days*24*3600)*106)/106 
+     if hasattr(value, "total_seconds"): return value.total_seconds() 
+     else: return (value.microseconds+(value.seconds+value.days*24*3600)*106)/106 
